c79c2dc986397bd94dc4c44277277e8739730d18
jerrysummer <reason2be@gmail.com>
2018-01-25 02:39:44 -0800

    Dialog box created on home

--- src/views/Home.js
+++ src/views/Home.js
@@ -5,6 +5,10 @@
 
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import Dialog from 'material-ui/Dialog';
+import FlatButton from 'material-ui/FlatButton';
+import RaisedButton from 'material-ui/RaisedButton';
+import DatePicker from 'material-ui/DatePicker';
 
 //-----------------------------------------------------------------------------------------
 //------------------------------------ Local imports --------------------------------------
@@ -12,6 +16,22 @@ import { connect } from 'react-redux';
 
 import './Home.css';
 import WorkyardLogo from '../assets/images/workyard-logo.svg';
+
+const customContentStyle = {
+  'width': '429px',
+  'height': '531px',
+};
+
+const customTitleStyle = {
+  'width': '264px',
+  'height': '23px',
+  'font-size': '18px',
+  'color': 'rgb(57, 57, 57)',
+  'margin': 'auto',
+  'text-align': 'center',
+  'font-weight': 'regular',
+};
+
 
 //-----------------------------------------------------------------------------------------
 //------------------------------------ Home Component -------------------------------------
@@ -26,13 +46,23 @@ class Home extends Component {
   
   constructor(props) {
     super(props);
-    this.state = {}
+    this.state = {
+      open: false
+    }
     this.handlerExample = this.handlerExample.bind(this);
   }
 
   //-------------------------------------------------------------------------
   //------------------------- Handler methods -------------------------------
   //-------------------------------------------------------------------------
+
+  handleOpen = () => {
+    this.setState({ open: true });
+  };
+
+  handleClose = () => {
+    this.setState({ open: false });
+  };
 
   handlerExample() {
     console.log("Handler Example Running!");
@@ -43,6 +73,14 @@ class Home extends Component {
   //-------------------------------------------------------------------------
   
   render() {
+    const actions = [
+      <FlatButton
+        label="Ok"
+        primary={true}
+        keyboardFocused={true}
+        onClick={this.handleClose}
+      />,
+    ];
 
     const { handlerExample } = this;
 
@@ -50,7 +88,23 @@ class Home extends Component {
       <div className="home-container">
         <img src={WorkyardLogo} alt="Workyard logo" className="workyard-logo"/>
         <h1>Post a project</h1>
-        <button onClick={handlerExample}>Create Project</button>
+        <button onClick={this.handleOpen}>Create Project</button>
+
+        <div>
+          {/* <RaisedButton label="Dialog With Date Picker" onClick={this.handleOpen} /> */}
+          <Dialog
+            title="Add a project you've worked"
+            actions={actions}
+            modal={false}
+            contentStyle={customContentStyle}
+            titleStyle={customTitleStyle}
+            open={this.state.open}
+            onRequestClose={this.handleClose}
+          >
+          <DatePicker hintText="Date Picker" />
+          </Dialog>
+        </div>
+        
       </div>
     );
   }
