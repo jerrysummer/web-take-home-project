f5c2280983f1a5d7867b6e70b8c8de267dd52aac
jerrysummer <reason2be@gmail.com>
2018-01-25 03:42:57 -0800

    Home div fixed to size of window

--- src/views/Home.js
+++ src/views/Home.js
@@ -7,8 +7,10 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Dialog from 'material-ui/Dialog';
 import FlatButton from 'material-ui/FlatButton';
-import RaisedButton from 'material-ui/RaisedButton';
-import DatePicker from 'material-ui/DatePicker';
+// import RaisedButton from 'material-ui/RaisedButton';
+import SelectField from 'material-ui/SelectField';
+import MenuItem from 'material-ui/MenuItem';
+import TextField from 'material-ui/TextField';
 
 //-----------------------------------------------------------------------------------------
 //------------------------------------ Local imports --------------------------------------
@@ -25,13 +27,27 @@ const customContentStyle = {
 const customTitleStyle = {
   'width': '264px',
   'height': '23px',
-  'font-size': '18px',
+  fontSize: '18px',
   'color': 'rgb(57, 57, 57)',
   'margin': 'auto',
-  'text-align': 'center',
-  'font-weight': 'regular',
+  textAlign: 'center',
+  fontWeight: 'regular',
 };
 
+const items = [
+  <MenuItem key={1} value={1} primaryText="Never" />,
+  <MenuItem key={2} value={2} primaryText="Every Night" />,
+  <MenuItem key={3} value={3} primaryText="Weeknights" />,
+  <MenuItem key={4} value={4} primaryText="Weekends" />,
+  <MenuItem key={5} value={5} primaryText="Weekly" />,
+];
+const sizes = [
+  <MenuItem key={1} value={1} primaryText="Never" />,
+  <MenuItem key={2} value={2} primaryText="Every Night" />,
+  <MenuItem key={3} value={3} primaryText="Weeknights" />,
+  <MenuItem key={4} value={4} primaryText="Weekends" />,
+  <MenuItem key={5} value={5} primaryText="Weekly" />,
+];
 
 //-----------------------------------------------------------------------------------------
 //------------------------------------ Home Component -------------------------------------
@@ -47,7 +63,11 @@ class Home extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      open: false
+      open: false,
+      projectType: null,
+      projectDescription: '',
+      contractSize: null,
+      location: '',
     }
     this.handlerExample = this.handlerExample.bind(this);
   }
@@ -62,6 +82,18 @@ class Home extends Component {
 
   handleClose = () => {
     this.setState({ open: false });
+  };
+
+  handleProjectTypeChange = (event, index, value) => {
+    this.setState({ projectType: value });
+  };
+
+  handleContractSizeChange = (event, index, value) => {
+    this.setState({ contractSize: value });
+  };
+
+  handleProjectDescriptionChange = (event, index, value) => {
+    this.setState({ projectDescription: event.target.value });
   };
 
   handlerExample() {
@@ -101,7 +133,37 @@ class Home extends Component {
             open={this.state.open}
             onRequestClose={this.handleClose}
           >
-          <DatePicker hintText="Date Picker" />
+            <SelectField
+              value={this.state.projectType}
+              onChange={this.handleProjectTypeChange}
+              floatingLabelText="Select project type"
+              fullWidth={true}
+            >
+              {items}
+            </SelectField><br />
+
+            <TextField
+              hintText="Add a project description"
+              id="text-field-controlled"
+              fullWidth={false}
+              value={this.state.projectDescription}
+              onChange={this.handleProjectDescriptionChange}
+              underlineShow={true}
+            /><br />
+
+            <SelectField
+              value={this.state.contractSize}
+              onChange={this.handleContractSizeChange}
+              fullWidth={false}
+              floatingLabelText='Select a contract size'
+              underlineShow={true}
+            >
+              {sizes}
+            </SelectField><br />
+
+            
+
+
           </Dialog>
         </div>
         
